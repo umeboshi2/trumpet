6e04be820d027a98cfdd80b230679f78d6f3cace
Joseph Rawson <joseph.rawson.works@littledebian.org>
2014-01-27 15:46:43 -0600

    updating

--- _plugins/jekyll_asset_pipeline.rb
+++ _plugins/jekyll_asset_pipeline.rb
@@ -1,3 +1,30 @@
 require 'jekyll_asset_pipeline'
 
 
+module JekyllAssetPipeline
+    class CoffeeScriptConverter < JekyllAssetPipeline::Converter
+      require 'coffee-script'
+
+      def self.filetype
+        '.coffee'
+      end
+
+      def convert
+        return CoffeeScript.compile(@content)
+      end
+    end
+end
+
+module JekyllAssetPipeline
+  class SassConverter < JekyllAssetPipeline::Converter
+    require 'sass'
+
+    def self.filetype
+      '.scss'
+    end
+
+    def convert
+      return Sass::Engine.new(@content, syntax: :scss).render
+    end
+  end
+end
