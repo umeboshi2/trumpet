9ec5746a33e3127d90bb0480d0fa81c47bcfd079
Joseph Rawson <joseph.rawson.works@littledebian.org>
2014-08-30 10:29:18 -0500

    explicitly close db sessions in request

--- trumpet/request.py
+++ trumpet/request.py
@@ -6,4 +6,13 @@ class AlchemyRequest(Request):
     @reify
     def db(self):
         maker = self.registry.settings['db.sessionmaker']
-        return maker()
+        session = maker()
+        def close_session(request):
+            if request.exception is not None:
+                session.rollback()
+            else:
+                session.commit()
+            session.close()
+        self.add_finished_callback(close_session)
+        return session
+    
