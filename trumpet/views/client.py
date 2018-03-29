77f9d4b24ce131bd95292b4c7bca7f9dd2a4e1cb
Joseph Rawson <joseph.rawson.works@gmail.com>
2018-03-29 18:20:54 -0500

    update client view

--- trumpet/views/client.py
+++ trumpet/views/client.py
@@ -8,14 +8,4 @@ class BaseClientView(BaseViewCallable):
     def __init__(self, request):
         super(BaseClientView, self).__init__(request)
         self.settings = self.get_app_settings()
-        self.data = dict(
-            basecolor=self.settings.get('default.css.basecolor', 'vanilla'),
-            csspath=self.settings.get(
-                'default.css.path', '/assets/stylesheets'),
-            jspath=self.settings.get('default.js.path', '/assets/client'),)
-
-    # @view_config(route_name='home')
-    # def index(self):
-    #    option = 'default.js.mainapp'
-    #    self.data['appname'] = self.settings.get(option, 'index')
-    #    return self.data
+        self.data = dict()
