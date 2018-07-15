94db3516b21d0dad9ed6bdfc145b4d9c5d5f3ed9
Joseph Rawson <joseph.rawson.works@gmail.com>
2018-07-15 10:18:42 -0500

    added simple resource views

--- trumpet/views/base.py
+++ trumpet/views/base.py
@@ -59,3 +59,41 @@ class BaseViewCallable(BaseView):
 
 class BaseUserViewCallable(BaseViewCallable, BaseUserView):
     pass
+
+
+class BaseResource(BaseViewCallable):
+    def __init__(self, request, context=None):
+        super(BaseResource, self).__init__(request)
+        if not hasattr(self, 'dbmodel'):
+            msg = "need to set dbmodel property before __init__"
+            raise RuntimeError(msg)
+
+    def _query(self):
+        return self.db.query(self.dbmodel)
+
+    def _get(self, id):
+        return self._query().get(id)
+
+    def get(self):
+        id = int(self.request.matchdict['id'])
+        return self._get(id).serialize()
+
+
+class BaseManagementResource(BaseResource):
+    def __init__(self, request, context=None):
+        super(BaseManagementResource, self).__init__(request)
+        if not hasattr(self, 'mgrclass'):
+            msg = "need to set mgrclass property before __init__"
+            raise RuntimeError(msg)
+        self.mgr = self.mgrclass(self.request.dbsession)
+        
+    def get(self):
+        id = int(self.request.matchdict['id'])
+        return self.mgr.get(id).serialize()
+    
+    def collection_get(self):
+        # FIXME: use offset, limit, and pass filters
+        objects = self.mgr.all()
+        return dict(data=[o.serialize() for o in objects])
+        
+        
