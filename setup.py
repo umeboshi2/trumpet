2ab91dc6aa2c719679faa8a79ccb2a8fd26091f4
Joseph Rawson <joseph.rawson.works@gmail.com>
2019-09-26 22:07:51 -0500

    Bump version: 0.2.10 → 0.2.11

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ test_requirements = [
 
 setup(
     name='trumpet',
-    version='0.2.10',
+    version='0.2.11',
     description="Build a website with pyramid",
     long_description=readme + '\n\n' + history,
     author="Joseph Rawson",
